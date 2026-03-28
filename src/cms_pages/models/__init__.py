66c0d3993675c02743ac496fdf67cb6177c25fef
Dmitriy <ViatkinDima@gmail.com>
2026-03-28 20:48:11 +0200

    Add template psges delivery and payment

--- src/cms_pages/models/__init__.py
+++ src/cms_pages/models/__init__.py
@@ -4,3 +4,4 @@ from .models import (Page404, HomePage, GalleryPage, DetailNewsPage,NewsListPage
 from .blocks import ContentBlock, BanerBlock, NewsBlock
 from .order_placed import OrderPlaced
 from .CorporateClientsPage import CorporateClientsPage
+from .delivery_payment import DeliveryPaymentPage
