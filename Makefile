6c25d1848bf6c7903be66d0a108ea1ca9344871e
Dmitriy <ViatkinDima@gmail.com>
2026-04-21 15:27:35 +0300

    Change entrypoint

--- Makefile
+++ Makefile
@@ -16,16 +16,14 @@ migrate:
 makemigrations:
 	$(PYTHON) $(MANAGE) makemigrations
 
-
 run_celery:
 	celery -A config worker -B -l info
 
-# Запуск Vite (предполагаем, что папка называется frontend)
 run_npm:
 	cd frontend && npm run dev
 
 m-migrate:
-	 make makemigrations migrate
+	make makemigrations migrate
 
 start_dev:
 	make -j3 run run_npm run_celery
@@ -37,9 +35,14 @@ stop_all:
 
 initial_data:
 	$(PYTHON) $(MANAGE) start_page
+
+s_user:
+	$(PYTHON) $(MANAGE) createsuperuser
+
 start_prod:
 	$(PYTHON) $(MANAGE) migrate wagtailcore && \
 	$(PYTHON) $(MANAGE) migrate cities_light && \
 	$(PYTHON) $(MANAGE) migrate && \
 	$(PYTHON) $(MANAGE) collectstatic --noinput && \
+	$(PYTHON) $(MANAGE) createsuperuser && \
 	gunicorn config.wsgi:application --bind 0.0.0.0:8000
--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -4,11 +4,28 @@ set -e
 echo "==> Migrating wagtailcore..."
 yes | python manage.py migrate wagtailcore
 
+echo "==> Migrating cities_light..."
+yes | python manage.py migrate cities_light
+
 echo "==> Migrating all..."
 yes | python manage.py migrate
 
 echo "==> Collecting static..."
 python manage.py collectstatic --noinput
+
+echo "==> Creating superuser..."
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = '${DJANGO_SUPERUSER_USERNAME:-admin}'
+email = '${DJANGO_SUPERUSER_EMAIL:-admin@example.com}'
+password = '${DJANGO_SUPERUSER_PASSWORD:-admin}'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f'Superuser \"{username}\" created.')
+else:
+    print(f'Superuser \"{username}\" already exists, skipping.')
+"
 
 echo "==> Starting gunicorn..."
 exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
