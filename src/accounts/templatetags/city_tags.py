cf17721613543e2b37772f1beeb9203f1f6fd965
Dmitriy <ViatkinDima@gmail.com>
2026-05-07 21:43:11 +0300

    Fix nam region

--- src/accounts/templatetags/city_tags.py
+++ src/accounts/templatetags/city_tags.py
@@ -1,8 +1,19 @@
+
+
+
 from django import template
 from django.utils.translation import get_language
 
 register = template.Library()
 
+def is_cyrillic(name):
+    return any('\u0400' <= c <= '\u04FF' for c in name)
+
+def is_russian(name):
+    return any(c in name for c in ['ы', 'э', 'ё', 'ъ', 'Ы', 'Э', 'Ё', 'Ъ'])
+
+def is_ascii(name):
+    return name.isascii()
 
 def get_name_by_lang(obj, lang):
     if not obj.alternate_names:
@@ -10,23 +21,23 @@ def get_name_by_lang(obj, lang):
 
     names = [n.strip() for n in obj.alternate_names.split(';') if n.strip()]
 
-    for name in names:
-        low = name.lower()
-
-        if lang == 'uk':
-            if any(c in low for c in ['і', 'ї', 'є', 'ґ']):
+    if lang == 'uk':
+        # Кирилиця без російських літер = українська
+        for name in names:
+            if is_cyrillic(name) and not is_russian(name):
                 return name
 
-        elif lang == 'ru':
-            if any(c in low for c in ['ы', 'э', 'ё']):
+    elif lang == 'ru':
+        for name in names:
+            if is_cyrillic(name) and is_russian(name):
                 return name
 
-        elif lang == 'en':
-            if name.isascii():
+    elif lang == 'en':
+        for name in names:
+            if is_ascii(name):
                 return name
 
     return obj.name
-
 
 @register.filter
 def localized_name(obj):
